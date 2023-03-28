6da8e9fc397d672ccf74d2767a81456e0acdd15d
xiejinfu <xringxie@gmail.com>
2023-03-28 18:17:45 +0800

    update for internal only

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -84,7 +84,7 @@ export const Chat: FC<Props> = ({ conversation, models, apiKey, isUsingEnv, mess
       ) : modelError ? (
         <div className="flex flex-col justify-center mx-auto h-full w-[300px] sm:w-[500px] space-y-6">
           <div className="text-center text-red-500">Gitee Token Invalid.</div>
-          <div className="text-center text-red-500">"emails" and "enterprises" should be checked when get a Gitee token.</div>
+          <div className="text-center text-red-500">emails and enterprises should be checked when get a Gitee token.</div>
           <div className="text-center text-red-500">Get token at https://gitee.com/profile/personal_access_tokens</div>
         </div>
       ) : (
