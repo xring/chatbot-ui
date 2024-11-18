35718a358d2715e9fa423051497172ed1c877918
xring <xringxie@gmail.com>
2024-11-18 21:06:40 +0800

    try using yjzf-llm-1118

--- utils/app/clean.ts
+++ utils/app/clean.ts
@@ -6,6 +6,7 @@ export const cleanSelectedConversation = (conversation: Conversation) => {
   // added system prompt for each conversation (3/21/23)
   // added folders (3/23/23)
 
+
   let updatedConversation = conversation;
 
   // check for model on each conversation
